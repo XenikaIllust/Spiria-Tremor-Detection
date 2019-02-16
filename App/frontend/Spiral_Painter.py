c52bc43ba4773725249439d7d91af58566c970f3
Zachary Lim <limz@purdue.edu>
2019-02-16 15:03:46 -0500

    Working on Spiral Painter

--- App/frontend/Spiral_Painter.py
+++ App/frontend/Spiral_Painter.py
@@ -7,41 +7,55 @@ class Spiral_Painter(QWidget):
     def __init__(self, parent=None):
         super().__init__()
 
+        self.setup_ui()
+
         self.draw_enabled = False
 
-        self.image = QImage()
+        self.pixmap = QPixmap(self.width(), self.height())
+        self.label = QLabel()
+        self.label.setPixmap(self.pixmap)
+        self.last_pos = None
 
         self.painter = QPainter()
-        self.painter.begin(self.image)
-        self.painter.setPen(Qt.red)
-        self.painter.end()
 
     def setup_ui(self):
-        self.geometry(QRect(400, 400, 400, 400))
+        self.setGeometry(QRect(400, 400, 400, 400))
+        self.show()
+
+    def paintEvent(self, event):
+        if self.last_pos != None:
+            self.painter.begin(self.pixmap)
+            self.painter.setPen(Qt.red)
+            self.painter.pen().setWidth(10)
+            self.painter.drawEllipse(self.last_pos, 50, 50)
+            self.painter.end()
+            self.label.setPixmap(self.pixmap)
+            print("canvas repainted")
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.draw_enabled = True
-            self.painter.begin(self.image)
-            self.painter.drawPoint(event.pos())
-            self.painter.end()
+            print("Mouse press: ", event.pos())
+            # self.painter.drawPoint(event.pos())
+            self.last_pos = event.pos()
+            #self.repaint()
+            self.update()
 
     def mouseMoveEvent(self, event):
-        if event.button() == Qt.LeftButton and self.draw_enabled:
-            self.painter.begin(self.image)
-            self.painter.drawPoint(event.pos())
-            self.painter.end()
+        if self.draw_enabled:
+            print("Mouse move: ", event.pos())
+            # self.painter.drawPoint(event.pos())
+            self.last_pos = event.pos()
+            #self.repaint()
+            self.update()
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton and self.draw_enabled:
+            print("Mouse release: ", event.pos())
             self.draw_enabled = False
-
-
-
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     spiral_painter = Spiral_Painter()
-    spiral_painter.show()
     app.exec_()
