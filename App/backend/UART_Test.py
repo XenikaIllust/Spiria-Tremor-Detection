b5fa28359888b25b4c654b544a209f27e440b045
Zachary Lim <limz@purdue.edu>
2019-03-01 16:33:37 -0500

    Testing UART plotting

--- App/backend/UART_Test.py
+++ App/backend/UART_Test.py
@@ -1,14 +1,14 @@
 from UART_Handler import *
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 import time
 
 if __name__ == "__main__":
-    uart_handler = UART_Handler("/dev/ttyS0", 9600)
+    # uart_handler = UART_Handler("/dev/ttyS0", 9600)
     # status = uart_handler.pairing()
 
-    fig = plt.gcf()
-
+    """
     x_pt = 0
     y_pt = 0
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     h1, = plt.plot([], [])
 
     pts = None
-
+    
     while True:
         point = uart_handler.get_point()
         
@@ -37,3 +37,28 @@ if __name__ == "__main__":
 
         plt.draw()
         plt.pause(0.0001)
+    """
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+
+    ax1.set_xlim([0, 1023])
+    ax1.set_ylim([0, 1023])
+
+    def animate(i):
+        point = uart_handler.get_point()
+
+        x_pt = point[0]
+        y_pt = point[1]
+
+        # if pts != None:
+        #     pts.remove()
+
+        ax1.clear()
+        ax1.scatter(x_pt, y_pt, c="r")
+        ax1.set_xlim([0, 1023])
+        ax1.set_ylim([0, 1023])
+
+
+    ani = animation.FuncAnimation(fig, animate, interval=1)
+    plt.show()
